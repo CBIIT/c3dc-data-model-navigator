2d7f2df989387506c224b23cc111fb76c2765443
Chen <jonkiky@gmail.com>
2023-04-13 14:46:06 -0400

    add build file

---
+++ docs/precache-manifest.d8c81ef716241502c943657fa85aa68b.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = [
+  {
+    "revision": "0db9222230755f84c962",
+    "url": "/c3dc-data-model-navigator/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0db9222230755f84c962",
+    "url": "/c3dc-data-model-navigator/static/js/main.0db92222.chunk.js"
+  },
+  {
+    "revision": "62f3220776a7e628d1a4",
+    "url": "/c3dc-data-model-navigator/static/js/runtime~main.62f32207.js"
+  },
+  {
+    "revision": "79a21cb8b8f9df30048b",
+    "url": "/c3dc-data-model-navigator/static/css/2.c9528835.chunk.css"
+  },
+  {
+    "revision": "79a21cb8b8f9df30048b",
+    "url": "/c3dc-data-model-navigator/static/js/2.79a21cb8.chunk.js"
+  },
+  {
+    "revision": "41a250e379ae7384227b",
+    "url": "/c3dc-data-model-navigator/static/js/3.41a250e3.chunk.js"
+  },
+  {
+    "revision": "3fcb9c79cd58e79665c4",
+    "url": "/c3dc-data-model-navigator/static/js/4.3fcb9c79.chunk.js"
+  },
+  {
+    "revision": "b9885507ccc3b742ee12",
+    "url": "/c3dc-data-model-navigator/static/js/5.b9885507.chunk.js"
+  },
+  {
+    "revision": "5ef5f772b0579320f5a789ab8cb39dd2",
+    "url": "/c3dc-data-model-navigator/static/media/browser.5ef5f772.cjs"
+  },
+  {
+    "revision": "0fc4b6539307f12e170fef83897a908c",
+    "url": "/c3dc-data-model-navigator/static/media/CTDC_Logo.0fc4b653.svg"
+  },
+  {
+    "revision": "a21a2ca7aeb68c268745ad8e395c7013",
+    "url": "/c3dc-data-model-navigator/static/media/Model-Buttons-Center.a21a2ca7.svg"
+  },
+  {
+    "revision": "831e1a08695a94a2514f0988816074a2",
+    "url": "/c3dc-data-model-navigator/static/media/Model-Buttons-ZoomIn.831e1a08.svg"
+  },
+  {
+    "revision": "c09b754e0eafb703ddb5f808e66b8422",
+    "url": "/c3dc-data-model-navigator/static/media/Model-Buttons-ZoomOut.c09b754e.svg"
+  },
+  {
+    "revision": "79bff0a1361235a2e46e8d930ba82e64",
+    "url": "/c3dc-data-model-navigator/static/media/About-ExternalLink.79bff0a1.svg"
+  },
+  {
+    "revision": "fd9781121ca9b4107e18de72edffcf36",
+    "url": "/c3dc-data-model-navigator/static/media/About-Table-ExternalLink.fd978112.svg"
+  },
+  {
+    "revision": "1a39215332dafa258d68fe1f72b46d37",
+    "url": "/c3dc-data-model-navigator/static/media/study.1a392153.svg"
+  },
+  {
+    "revision": "a4ae8729dd47df44ad85a18045fac8ac",
+    "url": "/c3dc-data-model-navigator/static/media/case.a4ae8729.svg"
+  },
+  {
+    "revision": "18449bf1dc4516fdbad16ae4227bf9e9",
+    "url": "/c3dc-data-model-navigator/static/media/clinical_trial.18449bf1.svg"
+  },
+  {
+    "revision": "33d112a1fbc805512f05c9773022e68b",
+    "url": "/c3dc-data-model-navigator/static/media/administrative.33d112a1.svg"
+  },
+  {
+    "revision": "4df6b0486441b2b49668c08579b97549",
+    "url": "/c3dc-data-model-navigator/static/media/biospecimen.4df6b048.svg"
+  },
+  {
+    "revision": "599e70d1a1ccd38a9d9014431d192125",
+    "url": "/c3dc-data-model-navigator/static/media/analysis.599e70d1.svg"
+  },
+  {
+    "revision": "6a4b8e0787c7289aa07311d00c31df7f",
+    "url": "/c3dc-data-model-navigator/static/media/data_file.6a4b8e07.svg"
+  },
+  {
+    "revision": "76f3e8c84b250329e029d6fb7d081dba",
+    "url": "/c3dc-data-model-navigator/static/media/clinical.76f3e8c8.svg"
+  },
+  {
+    "revision": "d1e5fad76b1b8f8af2f43a9babe4a4ec",
+    "url": "/c3dc-data-model-navigator/static/media/lg_study.d1e5fad7.svg"
+  },
+  {
+    "revision": "dbfb54720de25e2a2c229a6ee529b64b",
+    "url": "/c3dc-data-model-navigator/static/media/lg_case.dbfb5472.svg"
+  },
+  {
+    "revision": "68d9afd90ade16e436483c5ffff53871",
+    "url": "/c3dc-data-model-navigator/static/media/lg_clinical_trial.68d9afd9.svg"
+  },
+  {
+    "revision": "d84b03b06d1afaa96a7a7518634d26a9",
+    "url": "/c3dc-data-model-navigator/static/media/lg_administrative.d84b03b0.svg"
+  },
+  {
+    "revision": "9ea62dabdd638f58e95b5b14630429f8",
+    "url": "/c3dc-data-model-navigator/static/media/lg_biospecimen.9ea62dab.svg"
+  },
+  {
+    "revision": "86585179735b1a4fba29fb2ca4f6d569",
+    "url": "/c3dc-data-model-navigator/static/media/lg_analysis.86585179.svg"
+  },
+  {
+    "revision": "cca150b3972e90ad29faa999570c8403",
+    "url": "/c3dc-data-model-navigator/static/media/lg_data_file.cca150b3.svg"
+  },
+  {
+    "revision": "683723cde2d7756c3e62a101bc4b7d74",
+    "url": "/c3dc-data-model-navigator/static/media/lg_clinical.683723cd.svg"
+  },
+  {
+    "revision": "7a8925289e08c3ca04cd144c1ba3f739",
+    "url": "/c3dc-data-model-navigator/static/media/icon_default.7a892528.svg"
+  },
+  {
+    "revision": "78bf361d456e45b4ba0ab9731ffa293a",
+    "url": "/c3dc-data-model-navigator/static/media/Nunito-ExtraBold.78bf361d.ttf"
+  },
+  {
+    "revision": "6b6424c109a32ec71c9b785202ed54c2",
+    "url": "/c3dc-data-model-navigator/static/media/Nunito-Bold.6b6424c1.ttf"
+  },
+  {
+    "revision": "ec1377dc527a55ee27635376679e6a09",
+    "url": "/c3dc-data-model-navigator/static/media/Nunito-SemiBold.ec1377dc.ttf"
+  },
+  {
+    "revision": "44d748e5db30636aa7a9d1342f591b4d",
+    "url": "/c3dc-data-model-navigator/static/media/Nunito-ExtraLightItalic.44d748e5.ttf"
+  },
+  {
+    "revision": "3783b73c7d1f3c0d6c6cecbc47837553",
+    "url": "/c3dc-data-model-navigator/static/media/Nunito-Medium.3783b73c.ttf"
+  },
+  {
+    "revision": "c15f495cec2eea90bb73b1ebf6468943",
+    "url": "/c3dc-data-model-navigator/static/media/Nunito-Light.c15f495c.ttf"
+  },
+  {
+    "revision": "0e39fe3252a04fd31100576aa24c2e2f",
+    "url": "/c3dc-data-model-navigator/static/media/Model_View.0e39fe32.png"
+  },
+  {
+    "revision": "14b880ae679bf11ed7933b904f9f87ae",
+    "url": "/c3dc-data-model-navigator/static/media/key_icon.14b880ae.svg"
+  },
+  {
+    "revision": "0f05a0550057dd754cdf38bbd3d25c5b",
+    "url": "/c3dc-data-model-navigator/static/media/icon_download_PDF.0f05a055.svg"
+  },
+  {
+    "revision": "cb3cb029bd2375586f9b509ef93ef1e0",
+    "url": "/c3dc-data-model-navigator/static/media/icon_download_TSV.cb3cb029.svg"
+  },
+  {
+    "revision": "706b4194bbc278241a139bcf3f78ad6b",
+    "url": "/c3dc-data-model-navigator/static/media/lg_relationship_links.706b4194.svg"
+  },
+  {
+    "revision": "eb03283bf9ad9ed5780b97294ee2e237",
+    "url": "/c3dc-data-model-navigator/static/media/lg_link.eb03283b.svg"
+  },
+  {
+    "revision": "39a3791a971cedc348d8a89fca03f362",
+    "url": "/c3dc-data-model-navigator/static/media/Reset.39a3791a.svg"
+  },
+  {
+    "revision": "3c946af24dcf8ef6bd63443387c8cd13",
+    "url": "/c3dc-data-model-navigator/static/media/ZoomIn.3c946af2.svg"
+  },
+  {
+    "revision": "fdd5afedfdfba924f8e44572f4f32e03",
+    "url": "/c3dc-data-model-navigator/static/media/ZoomOut.fdd5afed.svg"
+  },
+  {
+    "revision": "0cf06eef084f1d0e1ceb16aa1971103d",
+    "url": "/c3dc-data-model-navigator/static/media/study.0cf06eef.svg"
+  },
+  {
+    "revision": "1f71b996f42312621ffad318c6b1098a",
+    "url": "/c3dc-data-model-navigator/static/media/case.1f71b996.svg"
+  },
+  {
+    "revision": "fa6f734d56dfedf48170bc61d34825d2",
+    "url": "/c3dc-data-model-navigator/static/media/clinical_trial.fa6f734d.svg"
+  },
+  {
+    "revision": "cd2ddc6dcfe5df08ba5e0180cb7f075e",
+    "url": "/c3dc-data-model-navigator/static/media/administrative.cd2ddc6d.svg"
+  },
+  {
+    "revision": "148c015e7939b401182e7689adf267a7",
+    "url": "/c3dc-data-model-navigator/static/media/biospecimen.148c015e.svg"
+  },
+  {
+    "revision": "40622afdb3e3ee2b4e2ed445885eb21d",
+    "url": "/c3dc-data-model-navigator/static/media/analysis.40622afd.svg"
+  },
+  {
+    "revision": "3cee93f087b13b3415ef4a9f47f9d5a4",
+    "url": "/c3dc-data-model-navigator/static/media/data_file.3cee93f0.svg"
+  },
+  {
+    "revision": "b6db31c6c85fb8f85339a96efb8680a6",
+    "url": "/c3dc-data-model-navigator/static/media/clinical.b6db31c6.svg"
+  },
+  {
+    "revision": "b32984575042c65b7c57920cc8871bb5",
+    "url": "/c3dc-data-model-navigator/index.html"
+  }
+];
