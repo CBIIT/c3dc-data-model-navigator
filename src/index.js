2c94af83cfb00d0c4d1f8a5ba82308e7e2d6d7cf
rana22 <ambar.rana123@gmail.com>
2023-04-13 13:32:50 -0400

    configure webpack 4

--- src/index.js
+++ src/index.js
@@ -1,21 +1,36 @@
+// import React from 'react';
+// import ReactDOM, { createRoot } from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const rootElement = document.getElementById("root");
+// const root = createRoot(rootElement);
+// root.render(
+//   <>
+//     <Provider store={store}>
+//       <App />
+//    </Provider>
+//   </>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import store from './store';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './store';
+import { Provider } from 'react-redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
-   </Provider>
-  </React.StrictMode>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
